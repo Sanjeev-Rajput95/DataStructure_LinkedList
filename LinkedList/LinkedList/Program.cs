d89b62cc8ef687f132115f2b8824725c6b4ca5b7
sanjeev <sanjeevsingh.77d@gmail.com>
2022-11-09 02:45:05 +0530

    Inserted Element

--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("******************* WELCOME TO DATASTRUCTURE LINKEDLIST PROGRAM ***********************");
+            Console.WriteLine("******************* WELCOME TO DATASTRUCTURE LINKEDLIST PROGRAM *********************** \n");
 
             LinkedList l_Object = new LinkedList();
             l_Object.add(56);
-            l_Object.add(30);
             l_Object.add(70);
+            l_Object.display();
             l_Object.InsertAtParticularPosition(2, 30);
             l_Object.display();
 
